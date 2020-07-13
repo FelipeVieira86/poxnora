12ac2fc560120b3bbc4d85c4ec9c210b3d925097
LizzardMedeiros <luis.medeiros@outlook.com.br>
2020-07-13 01:33:27 -0300

    fixed api and styles

--- src/components/Championlist.js
+++ src/components/Championlist.js
@@ -1,66 +1,9 @@
 import React from 'react';
-// import ChampionsData from '../data/championsdata';
-// import Champion from './Champion';
 import Axios from 'axios';
 import ChampionInfo from './ChampionInfo';
 import {useAsync} from 'react-async';
 
 import './style.css';
-
-const demo = [
-  {
-    id: 18,
-    name: "Abomination",
-    description: "A mind is a terrible thing to waste. So is a splee…a kidney, a liver, an eye... - Xulos, Undead Sage",
-    maxRng: 1,
-    minRng: 1,
-    abilitySets: [],
-    allowRanked: true,
-    artist: "James Ryman",
-    classes: ["Brute"],
-    damage: 12,
-    deckLimit: 2,
-    defense: 2,
-    factions: ["Forsaken Wastes"],
-    forSale: true,
-    hash: "cj9Ej9Dj9CG2BG2Aj9Cj9Dj9Eeeeerzzgvqhtmnj",
-    hitPoints: 55,
-    noraCost: 70,
-    races: ["Undead", "Zombie"],
-    rarity: "UNCOMMON",
-    runeSet: "Pox Nora Release Set",
-    size: "1x1",
-    speed: 6,
-    startingAbilities: [],
-    tradeable: true,
-  },
-  {
-    id: 18,
-    name: "Abomination",
-    description: "A mind is a terrible thing to waste. So is a splee…a kidney, a liver, an eye... - Xulos, Undead Sage",
-    maxRng: 1,
-    minRng: 1,
-    abilitySets: [],
-    allowRanked: true,
-    artist: "James Ryman",
-    classes: ["Brute"],
-    damage: 12,
-    deckLimit: 2,
-    defense: 2,
-    factions: ["Forsaken Wastes"],
-    forSale: true,
-    hash: "cj9Ej9Dj9CG2BG2Aj9Cj9Dj9Eeeeerzzgvqhtmnj",
-    hitPoints: 55,
-    noraCost: 70,
-    races: ["Undead", "Zombie"],
-    rarity: "UNCOMMON",
-    runeSet: "Pox Nora Release Set",
-    size: "1x1",
-    speed: 6,
-    startingAbilities: [],
-    tradeable: true,
-  }
-];
 
 const getChamps = async () => {
   return new Promise(async (resolved, rejected) => {
