dc414107cc967207622da4dbd90890efbfb1ac1f
Felipe Vieira <flpvieira86@gmail.com>
2021-03-12 14:35:29 -0300

    Updates

---
+++ precache-manifest.1932213be310b8fc67594945783bb6ec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7fd7f8f956750d85812baa28e9d5d21",
+    "url": "/poxsite/index.html"
+  },
+  {
+    "revision": "80aa5e3ae55ed6004641",
+    "url": "/poxsite/static/css/2.5a510433.chunk.css"
+  },
+  {
+    "revision": "7d14fb3c62777d878f98",
+    "url": "/poxsite/static/css/main.18dc0b9c.chunk.css"
+  },
+  {
+    "revision": "80aa5e3ae55ed6004641",
+    "url": "/poxsite/static/js/2.aff8960d.chunk.js"
+  },
+  {
+    "revision": "d0b30a1909a753c393e134caf98fa9bb",
+    "url": "/poxsite/static/js/2.aff8960d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d14fb3c62777d878f98",
+    "url": "/poxsite/static/js/main.02861e06.chunk.js"
+  },
+  {
+    "revision": "7f4c25ee249670de41a0",
+    "url": "/poxsite/static/js/runtime-main.f6876b61.js"
+  }
+]);
